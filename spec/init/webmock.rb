4ac4da44a68c54d33e650b2835d37d6022a64562
timpeat <tim.peat@digital.justice.gov.uk>
2022-10-25 10:00:50 +0100

    Remove webmock debugging

--- spec/init/webmock.rb
+++ spec/init/webmock.rb
@@ -2,7 +2,3 @@
 #
 driver_urls = Webdrivers::Common.subclasses.map(&:base_url)
 WebMock.disable_net_connect!(allow_localhost: true, allow: driver_urls)
-
-WebMock.after_request do |request_signature, response|
-  puts "Request #{request_signature} was made and #{response} was returned"
-end
