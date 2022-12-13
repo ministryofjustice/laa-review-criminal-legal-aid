b64b4589982274757033984e0cf9028a6d7db4f1
Arthur Ashman <arthur.ashman@digital.justice.gov.uk>
2022-12-13 15:35:37 +0000

    Account for situation where there is no next application to get

--- spec/system/get_next_application_spec.rb
+++ spec/system/get_next_application_spec.rb
@@ -7,4 +7,17 @@ RSpec.describe 'Assigning an application to myself' do
     expect(page).to have_content('Kit Pound')
     expect(page).to have_content('This application has been assigned to you')
   end
+
+  # rubocop:disable RSpec/ExampleLength
+  it 'shows an error when there is no next application' do
+    visit '/'
+    click_on 'Get next application'
+    visit '/'
+    click_on 'Get next application'
+    visit '/'
+    click_on 'Get next application'
+    expect(page).to have_content('Your list')
+    expect(page).to have_content('There are no unassigned applications to that need processing at this time')
+  end
+  # rubocop:enable RSpec/ExampleLength
 end
