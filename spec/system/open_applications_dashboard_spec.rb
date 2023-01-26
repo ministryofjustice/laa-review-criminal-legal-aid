8f4502939a46b3fe8687a29039fda58d0099e684
timpeat <tim@timpeat.com>
2023-01-26 12:45:50 +0000

    CRIMRE-123 rename Time passed' 'Days passed' (#100)
    also fixes search results page with returned applications.

--- spec/system/open_applications_dashboard_spec.rb
+++ spec/system/open_applications_dashboard_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe 'Open Applications Dashboard' do
   it 'includes the correct headings' do
     column_headings = page.first('.app-dashboard-table thead tr')
 
-    expect(column_headings.text).to eq('Applicant Ref. no. Date received Time passed Common Platform Caseworker')
+    expect(column_headings.text).to eq('Applicant Ref. no. Date received Days passed Common Platform Caseworker')
   end
 
   it 'shows the correct information' do
