87ab2f3db8d1284c188045456145457ad9c91480
timpeat <tim.peat@digital.justice.gov.uk>
2023-01-26 17:10:17 +0000

    CRIMRE-142 Add sorting buttons (WIP)
    Co-authored-by: willmcb <william.mcbrien@digital.justice.gov.uk>

--- app/controllers/application_searches_controller.rb
+++ app/controllers/application_searches_controller.rb
@@ -1,11 +1,14 @@
 class ApplicationSearchesController < ApplicationController
   def new
     @filter = ApplicationSearchFilter.new
+    @sorting = Sorting.new
   end
 
   def create
     @filter = ApplicationSearchFilter.new(search_params)
-    @search = ApplicationSearch.new(filter: @filter)
+    @sorting = Sorting.new(sort_params)
+
+    @search = ApplicationSearch.new(filter: @filter, sorting: @sorting)
 
     render :show
   end
@@ -20,6 +23,10 @@ class ApplicationSearchesController < ApplicationController
   private
 
   def search_params
-    params[:filter].permit(ApplicationSearchFilter.attribute_names)
+    params[:search][:application_search_filter].permit(ApplicationSearchFilter.attribute_names)
+  end
+
+  def sort_params
+    params[:search][:sorting]&.permit(Sorting.attribute_names)
   end
 end
