f66fbc5b7b914cad63042e46e52ba2b190df02fb
timpeat <tim.peat@digital.justice.gov.uk>
2023-01-26 17:52:02 +0000

    Remove search download

--- app/models/sorting.rb
+++ app/models/sorting.rb
@@ -1,7 +1,8 @@
 class Sorting < ApplicationStruct
-  attribute? :sort_direction, Types::String.default('descending').enum('descending', 'ascending')
+  attribute? :sort_direction, Types::String.default('descending'.freeze).enum('descending', 'ascending')
   attribute? :sort_by,
-             Types::String.default('submitted_at').enum('submitted_at', 'reviewed_at', 'reference', 'applicant_name')
+             Types::String.default('submitted_at'.freeze).enum('submitted_at', 'reviewed_at', 'reference',
+                                                               'applicant_name')
 
   def reverse_direction
     return 'ascending' if sort_direction == 'descending'
