a99a0444c4f53dcf2faf91b9a93cf77da6553a60
timpeat <tim@timpeat.com>
2023-02-14 14:31:50 +0000

    Crimre 166 use event stream for processed report (#121)
    * Use event data for processed report.
    
    * Assert only closing events counted.

---
+++ spec/models/reporting/processed_report_spec.rb
@@ -0,0 +1,56 @@
+require 'rails_helper'
+RSpec.configure do |config|
+  config.include ActiveSupport::Testing::TimeHelpers
+end
+
+describe Reporting::ProcessedReport do
+  subject(:report) { described_class.new }
+
+  describe '#table' do
+    it 'returns a table of report data' do
+      expect(report.table).to be_a Reporting::Table
+    end
+
+    it 'has column headers' do
+      expect(report.table.headers).to eq(['Closed on', 'Closed applications'])
+    end
+
+    it '3 rows of data' do
+      expect(report.table.rows.size).to eq(3)
+    end
+
+    it 'row headers with "Today", "Yesterday", and "Day before yesterday"' do
+      expect(report.table.rows.map(&:first).map(&:content)).to eq(['Today', 'Yesterday', 'Day before yesterday'])
+    end
+
+    describe 'data' do
+      subject(:data) { report.table.rows.map(&:last).map(&:content) }
+
+      before do
+        event_store = Rails.configuration.event_store
+
+        # Yesterday
+        travel_to Time.zone.now.yesterday
+        event_store.publish(Reviewing::Completed.new)
+
+        # The day before yesterday
+        travel_to Time.zone.now.yesterday
+        event_store.publish(Reviewing::SentBack.new)
+
+        # The day before, the day before yesterday
+        travel_to Time.zone.now.yesterday
+        event_store.publish(Reviewing::Completed.new)
+
+        # Today
+        travel_back
+        event_store.publish(Reviewing::ApplicationReceived.new)
+        event_store.publish(Reviewing::Completed.new)
+        event_store.publish(Reviewing::SentBack.new)
+      end
+
+      it 'includes a count of closing events for each day' do
+        expect(data).to eq([2, 1, 1])
+      end
+    end
+  end
+end
