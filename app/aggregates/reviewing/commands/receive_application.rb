53d09394140e28ae895d3e92cdebf20496c98195
timpeat <tim@timpeat.com>
2023-02-17 16:06:36 +0000

    CRIMRE-177 Spike sns callback for submission events (#133)

--- app/aggregates/reviewing/commands/receive_application.rb
+++ app/aggregates/reviewing/commands/receive_application.rb
@@ -1,10 +1,11 @@
 module Reviewing
   class ReceiveApplication < Command
     attribute :application_id, Types::Uuid
+    attribute? :correlation_id, Types::Uuid.optional
 
     def call
       with_review do |review|
-        review.receive_application(application_id:)
+        review.receive_application(application_id:, correlation_id:)
       end
     end
   end
