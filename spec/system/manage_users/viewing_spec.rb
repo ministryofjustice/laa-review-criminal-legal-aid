be3e115a219742c3ee481cb05e378009e91225e3
willmcb <william.mcbrien@digital.justice.gov.uk>
2023-03-02 15:59:59 +0000

    Add basic manage users viewing spec

---
+++ spec/system/manage_users/viewing_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe 'Manage Users Dashboard' do
+  include_context 'with a logged in user'
+
+  before do
+    visit '/admin/manage_users'
+  end
+
+  it 'includes the page heading' do
+    expect(page).to have_content 'Manage users'
+  end
+
+  # it 'includes the button to add new user' do
+  #   expect(page).to have_content 'Add new user'
+  # end
+  #
+  # it 'includes the correct headings' do
+  #   column_headings = page.first('.app-manage-users-table thead tr').text.squish
+  #
+  #   expect(column_headings).to eq('Email Manage other users Actions')
+  # end
+  #
+  # it 'shows the correct information' do
+  #   expect(page).to have_content('Joe.EXAMPLE@justice.gov.uk')
+  # end
+end
