ba6b7a0ebef6c941adefe4776fb4ee49977568a1
Arthur Ashman <arthur.ashman@digital.justice.gov.uk>
2023-03-02 17:58:30 +0000

    fix test, implement PR suggestions

--- spec/system/header_nav_spec.rb
+++ spec/system/header_nav_spec.rb
@@ -5,16 +5,9 @@ RSpec.describe 'Header navigation' do
     visit '/'
   end
 
-  it 'takes you to "Your list" when you click "Applications"' do
-    click_link('Applications')
-
-    heading_text = page.first('.govuk-heading-xl').text
-    expect(heading_text).to eq('Your list')
-  end
-
   it 'shows name of current user' do
-    header_navigation = page.all('.govuk-header__navigation-item')
-    expect(header_navigation[1].text).to eq('Joe EXAMPLE')
+    current_user = page.first('.govuk-header__navigation-item').text
+    expect(current_user).to eq('Joe EXAMPLE')
   end
 
   # TODO: test correct navigation to manage users page
