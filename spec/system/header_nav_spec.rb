5e412fd75d6a8682048bdc008befa29de0cbef3f
Arthur Ashman <arthur.ashman@digital.justice.gov.uk>
2023-03-03 10:40:15 +0000

    Hook up link to new manage user route

--- spec/system/header_nav_spec.rb
+++ spec/system/header_nav_spec.rb
@@ -10,12 +10,10 @@ RSpec.describe 'Header navigation' do
     expect(current_user).to eq('Joe EXAMPLE')
   end
 
-  # TODO: test correct navigation to manage users page
   it 'takes you to user management dashboard when you click "Manage users"' do
     click_link('Manage users')
 
     heading_text = page.first('.govuk-heading-xl').text
-    expect(heading_text).to eq('Your list')
-    # expect(heading_text).to eq('Manage users')
+    expect(heading_text).to eq('Manage users')
   end
 end
