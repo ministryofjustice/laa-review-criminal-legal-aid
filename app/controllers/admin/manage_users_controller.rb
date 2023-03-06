cd1b60e155dffad04e183a3da5300a1aa5c9b6a6
Arthur Ashman <45827968+arthurashman@users.noreply.github.com>
2023-03-06 14:18:26 +0000

    Restrict users to see nav option and access user admin only if allowed (#157)

--- app/controllers/admin/manage_users_controller.rb
+++ app/controllers/admin/manage_users_controller.rb
@@ -1,7 +1,13 @@
 module Admin
   class ManageUsersController < ApplicationController
     def index
-      @users = User.all
+      if current_user.can_manage_others
+        @users = User.all
+      else
+        flash[:important] = :cannot_access
+
+        redirect_to assigned_applications_path
+      end
     end
   end
 end
