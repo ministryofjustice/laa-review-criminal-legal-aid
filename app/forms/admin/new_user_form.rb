ea5d793f8b686dda2d86d29a270725218df3b18b
willmcb <william.mcbrien@digital.justice.gov.uk>
2023-03-07 12:07:37 +0000

    fix add user spec

--- app/forms/admin/new_user_form.rb
+++ app/forms/admin/new_user_form.rb
@@ -7,12 +7,20 @@ module Admin
     attribute :can_manage_others, :boolean
 
     validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-    validates :can_manage_others, presence: false
+    validates :can_manage_others, presence: false, inclusion: [true, nil]
 
     def save
       return unless valid?
 
-      user = User.new(email:, can_manage_others:)
+      # TODO: look at getting boolean from form directly
+      # seems to be issue with DFE form builder
+      boolean_can_manage_others = can_manage_others ? true : false
+
+      user = User.new(
+        email: email,
+        can_manage_others: boolean_can_manage_others
+      )
+
       user.save
     end
   end
