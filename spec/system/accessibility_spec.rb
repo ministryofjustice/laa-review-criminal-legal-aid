e253489cb57324ce33a23a77b2d662fb42dde0d0
hibo.abdilaahi <51047911+hiboabd@users.noreply.github.com>
2023-03-23 15:26:07 +0000

    Uncomment tests

--- spec/system/accessibility_spec.rb
+++ spec/system/accessibility_spec.rb
@@ -12,79 +12,79 @@ RSpec.describe 'Accessibility' do
     visit '/'
   end
 
-  # it 'viewing assigned applications page has no axe detectible accessibility issues' do
-  #   expect(page).to be_axe_clean.according_to accessibility_standards
-  # end
+  it 'viewing assigned applications page has no axe detectible accessibility issues' do
+    expect(page).to be_axe_clean.according_to accessibility_standards
+  end
 
-  # it 'open applications page has no axe detectible accessibility issues' do
-  #   visit open_crime_applications_path
-  #   expect(page).to be_axe_clean.according_to accessibility_standards
-  # end
+  it 'open applications page has no axe detectible accessibility issues' do
+    visit open_crime_applications_path
+    expect(page).to be_axe_clean.according_to accessibility_standards
+  end
 
-  # it 'closed applications page has no axe detectible accessibility issues' do
-  #   visit closed_crime_applications_path
-  #   expect(page).to be_axe_clean.according_to accessibility_standards
-  # end
+  it 'closed applications page has no axe detectible accessibility issues' do
+    visit closed_crime_applications_path
+    expect(page).to be_axe_clean.according_to accessibility_standards
+  end
 
-  # it 'auth failure page has no axe detectible accessibility issues' do
-  #   visit auth_failure_path
-  #   expect(page).to be_axe_clean.according_to accessibility_standards
-  # end
+  it 'auth failure page has no axe detectible accessibility issues' do
+    visit auth_failure_path
+    expect(page).to be_axe_clean.according_to accessibility_standards
+  end
 
-  # it 'application not found page has no axe detectible accessibility issues' do
-  #   visit application_not_found_path
-  #   expect(page).to be_axe_clean.according_to accessibility_standards
-  # end
+  it 'application not found page has no axe detectible accessibility issues' do
+    visit application_not_found_path
+    expect(page).to be_axe_clean.according_to accessibility_standards
+  end
 
-  # it 'unhandled page has no axe detectible accessibility issues' do
-  #   visit unhandled_path
-  #   expect(page).to be_axe_clean.according_to accessibility_standards
-  # end
+  it 'unhandled page has no axe detectible accessibility issues' do
+    visit unhandled_path
+    expect(page).to be_axe_clean.according_to accessibility_standards
+  end
 
-  # it 'forbidden page has no axe detectible accessibility issues' do
-  #   visit forbidden_path
-  #   expect(page).to be_axe_clean.according_to accessibility_standards
-  # end
+  it 'forbidden page has no axe detectible accessibility issues' do
+    visit forbidden_path
+    expect(page).to be_axe_clean.according_to accessibility_standards
+  end
 
-  # it 'application search page has no axe detectible accessibility issues' do
-  #   visit search_application_searches_path
-  #   expect(page).to be_axe_clean.according_to accessibility_standards
-  # end
+  it 'application search page has no axe detectible accessibility issues' do
+    visit search_application_searches_path
+    expect(page).to be_axe_clean.according_to accessibility_standards
+  end
 
-  # it 'application search results page has no axe detectible accessibility issues' do
-  #   visit new_application_searches_path
-  #   expect(page).to be_axe_clean.according_to accessibility_standards
-  # end
+  it 'application search results page has no axe detectible accessibility issues' do
+    visit new_application_searches_path
+    expect(page).to be_axe_clean.according_to accessibility_standards
+  end
 
-  # it 'assigned applications page has no axe detectible accessibility issues' do
-  #   visit assigned_applications_path
-  #   expect(page).to be_axe_clean.according_to accessibility_standards
-  # end
+  it 'assigned applications page has no axe detectible accessibility issues' do
+    visit assigned_applications_path
+    expect(page).to be_axe_clean.according_to accessibility_standards
+  end
 
-  # it 'manage user page has no axe detectible accessibility issues' do
-  #   visit admin_manage_users_path
-  #   expect(page).to be_axe_clean.according_to accessibility_standards
-  # end
+  it 'manage user page has no axe detectible accessibility issues' do
+    visit admin_manage_users_path
+    expect(page).to be_axe_clean.according_to accessibility_standards
+  end
 
-  # it 'add new user page has no axe detectible accessibility issues' do
-  #   visit new_admin_manage_user_path
-  #   expect(page).to be_axe_clean.according_to accessibility_standards
-  # end
+  it 'add new user page has no axe detectible accessibility issues' do
+    visit new_admin_manage_user_path
+    expect(page).to be_axe_clean.according_to accessibility_standards
+  end
 
-  # it 'deactivate user page has no axe detectible accessibility issues' do
-  #   visit new_admin_manage_user_deactivate_users_path(active_user)
-  #   expect(page).to be_axe_clean.according_to accessibility_standards
-  # end
+  it 'deactivate user page has no axe detectible accessibility issues' do
+    visit new_admin_manage_user_deactivate_users_path(active_user)
+    expect(page).to be_axe_clean.according_to accessibility_standards
+  end
 
-  # it 'edit user page has no axe detectible accessibility issues' do
-  #   visit edit_admin_manage_user_path(active_user)
-  #   expect(page).to be_axe_clean.according_to accessibility_standards
-  # end
+  it 'edit user page has no axe detectible accessibility issues' do
+    visit edit_admin_manage_user_path(active_user)
+    expect(page).to be_axe_clean.according_to accessibility_standards
+  end
 
-  # it 'reassign crime application page has no axe detectible accessibility issues' do
-  #   visit new_crime_application_reassign_path(crime_application_id)
-  #   expect(page).to be_axe_clean.according_to accessibility_standards
-  # end
+  it 'reassign crime application page has no axe detectible accessibility issues' do
+    visit new_crime_application_reassign_path(crime_application_id)
+    expect(page).to be_axe_clean.according_to accessibility_standards
+  end
 
   it 'return crime application page has no axe detectible accessibility issues' do
     visit new_crime_application_return_path(crime_application_id)
@@ -101,13 +101,13 @@ RSpec.describe 'Accessibility' do
     expect(page).to be_axe_clean.according_to accessibility_standards
   end
 
-  # it 'workload report page has no axe detectible accessibility issues' do
-  #   visit report_path(:workload_report)
-  #   expect(page).to be_axe_clean.according_to accessibility_standards
-  # end
+  it 'workload report page has no axe detectible accessibility issues' do
+    visit report_path(:workload_report)
+    expect(page).to be_axe_clean.according_to accessibility_standards
+  end
 
-  # it 'processed report page has no axe detectible accessibility issues' do
-  #   visit report_path(:processed_report)
-  #   expect(page).to be_axe_clean.according_to accessibility_standards
-  # end
+  it 'processed report page has no axe detectible accessibility issues' do
+    visit report_path(:processed_report)
+    expect(page).to be_axe_clean.according_to accessibility_standards
+  end
 end
