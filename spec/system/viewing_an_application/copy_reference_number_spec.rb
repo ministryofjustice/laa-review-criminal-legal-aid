351e76ab83fd176028c2bfe96c2650ee4ed90c23
Mohammed Hassan Latif <106679087+MohammedHassanLatif93@users.noreply.github.com>
2023-03-24 14:19:54 +0000

    Removed comment lines

--- spec/system/viewing_an_application/copy_reference_number_spec.rb
+++ spec/system/viewing_an_application/copy_reference_number_spec.rb
@@ -15,26 +15,20 @@ RSpec.describe 'Reference number' do
     expect(page).to have_content('Copy')
   end
 
-  describe 'clickling the link' do
+  describe 'clicking the link' do
     it 'copies the reference number to the system clipboard' do
-      # get the reference reference number from the page
-      case_reference = find('#reference-number').text
+      case_reference = find_by_id('reference-number').text
 
-      # grant spec access to clipboard in headless chrome
       page.driver.browser.execute_cdp(
         'Browser.grantPermissions',
         origin: page.server_url,
         permissions: ['clipboardReadWrite']
       )
 
-      # Click the link
       click_link 'Copy'
 
-      # Get the current value of the clipboard
-      # clipboard_value = page.evaluate_script('navigator.clipboard.readText()')
       clipboard_value = page.evaluate_async_script('navigator.clipboard.readText().then(arguments[0])')
 
-      # Test the clipboard value against an expected value
       expect(clipboard_value).to eq case_reference
     end
   end
