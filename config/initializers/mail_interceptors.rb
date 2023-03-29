fa0c4eb766e37c3639dc15147dcbabbc84d5da6e
willmcb <william.mcbrien@digital.justice.gov.uk>
2023-03-29 20:28:17 +0100

    Register NotifyMailerInterceptor differently

--- config/initializers/mail_interceptors.rb
+++ config/initializers/mail_interceptors.rb
@@ -1,8 +1,9 @@
 require_relative '../../app/lib/host_env'
 
 Rails.application.configure do
-  if HostEnv.env_name == 'staging'
-    config.action_mailer.interceptors = %w[NotifyMailerInterceptor]
+  if HostEnv.staging?
+    require_relative '../../app/lib/notify_mailer_interceptor'
+    ActionMailer::Base.register_interceptor(NotifyMailerInterceptor)
   end
 end
 
