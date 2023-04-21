c7a6c02ee607c5d29d4a6d47c6facec2eba5315c
timpeat <tim@timpeat.com>
2023-04-21 14:00:19 +0100

    CRIMRE 295 Ensure all applications are received (#230)
    Receive the application if it has not yet been received.
    
    In production, Review is notified of new applications via SNS.
    As such, applications should be "received" before being returned by the
    datastore.
    
    This PR makes sure that receive_if_required! is called when loading from
    the datastore to support other environments which do not have SNS set up,
    and to act as a failsafe should an application be returned by the datastore
    before its SNS event message has been processed.

--- spec/shared_contexts/with_an_existing_application.rb
+++ spec/shared_contexts/with_an_existing_application.rb
@@ -1,5 +1,9 @@
 RSpec.shared_context 'with an existing application', shared_context: :metadata do
   include_context 'with stubbed search'
 
+  before do
+    visit open_crime_applications_path
+  end
+
   let(:crime_application_id) { '696dd4fd-b619-4637-ab42-a5f4565bcf4a' }
 end
