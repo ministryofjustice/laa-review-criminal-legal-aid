3633d49121e26a9ab63db1d79af862a227e288ec
Jesus Laiz <zheileman@users.noreply.github.com>
2023-05-09 13:49:03 +0100

    Fix lograge payload current user method (#254)
    Follow up to PR #253.
    
    `current_user` is defined by Devise automagically.
    `current_user_id` is a private method in `ApplicationController` but the health controller is a `BareApplicationController` thus don't inherit this and other methods.
    
    Fix pods going into `CrashLoopBackOff` as the probe fails to assert the readiness of the pod.

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -26,7 +26,7 @@ Rails.application.configure do
 
   config.lograge.custom_payload do |controller|
     {
-      user_id: controller.current_user_id.to_param,
+      user_id: controller.current_user.to_param,
     }
   end
 end
