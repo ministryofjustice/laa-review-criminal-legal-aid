f2b3da70a08a1f7588497ad21ada657c57d6cfe6
hibo.abdilaahi <51047911+hiboabd@users.noreply.github.com>
2023-06-16 12:12:43 +0100

    Add shared context for a resubmitted application

--- spec/system/viewing_an_application/that_is_superseded_spec.rb
+++ spec/system/viewing_an_application/that_is_superseded_spec.rb
@@ -1,64 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe 'Viewing an application that is superseded' do
-  let(:application_id) { '696dd4fd-b619-4637-ab42-a5f4565bcf4a' }
-  let(:parent_id) { '47a93336-7da6-48ec-b139-808ddd555a41' }
+  include_context 'with resubmitted application'
+
   let(:latest_application_url) { "/applications/#{application_id}" }
 
-  let(:return_details) do
-    ReturnDetails.new(
-      reason: 'evidence_issue',
-      details: 'September bank statement required'
-    )
-  end
-
   before do
-    stub_request(
-      :get,
-      "#{ENV.fetch('DATASTORE_API_ROOT')}/api/v1/applications/#{application_id}"
-    ).to_return(body: LaaCrimeSchemas.fixture(1.0).read, status: 200)
-
-    Reviewing::ReceiveApplication.call(
-      application_id: parent_id,
-      submitted_at: '2022-10-24T09:50:04.000+00:00',
-      parent_id: nil
-    )
-
-    stub_request(
-      :get,
-      "#{ENV.fetch('DATASTORE_API_ROOT')}/api/v1/applications/#{parent_id}"
-    ).to_return(body: LaaCrimeSchemas.fixture(1.0, name: 'application_returned'), status: 200)
-
-    stub_request(
-      :put,
-      "#{ENV.fetch('DATASTORE_API_ROOT')}/api/v1/applications/#{parent_id}/return"
-    ).to_return(body: LaaCrimeSchemas.fixture(1.0, name: 'application_returned').read, status: 200)
-
-    user = User.create(
-      first_name: 'Fred',
-      last_name: 'Smitheg',
-      auth_oid: '976658f9-f3d5-49ec-b0a9-485ff8b308fa',
-      email: 'Fred.Smitheg@justice.gov.uk'
-    )
-
-    Assigning::AssignToUser.new(
-      user_id: user.id,
-      to_whom_id: user.id,
-      assignment_id: parent_id
-    ).call
-
-    Reviewing::SendBack.new(
-      user_id: user.id,
-      application_id: parent_id,
-      return_details: return_details.attributes
-    ).call
-
-    Reviewing::ReceiveApplication.call(
-      application_id: application_id,
-      submitted_at: '2022-10-24T09:50:04.000+00:00',
-      parent_id: parent_id
-    )
-
     visit '/'
     visit crime_application_path(parent_id)
   end
