56254bb39028da54082b8037fa684c9f65802878
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2023-06-19 14:24:24 +0100

    Remove fixtures (#313)

--- spec/init/api_data.rb
+++ spec/init/api_data.rb
@@ -1,10 +1,4 @@
 RSpec.configure do |config|
-  application_ids = %w[
-    1aa4c689-6fb5-47ff-9567-5eee7f8ac2cc
-    5aa4c689-6fb5-47ff-9567-5efe7f8ac211
-    148df27d-4710-4c5b-938c-bb132eb040ca
-  ]
-
   config.before do
     #
     # Stub for valid returned application for Kit Pound, fixture provided by the LaaCrimeSchemas Gem
@@ -13,20 +7,6 @@ RSpec.configure do |config|
       :get,
       "#{ENV.fetch('DATASTORE_API_ROOT')}/api/v1/applications/696dd4fd-b619-4637-ab42-a5f4565bcf4a"
     ).to_return(body: LaaCrimeSchemas.fixture(1.0).read, status: 200)
-    #
-    # Stub datastore find for ids listed in application_ids.
-    #
-    application_ids.each do |application_id|
-      raise 'Error! Cannot stub "Kit Pound" locally!' if application_id == '696dd4fd-b619-4637-ab42-a5f4565bcf4a'
-
-      stub_request(
-        :get,
-        "#{ENV.fetch('DATASTORE_API_ROOT')}/api/v1/applications/#{application_id}"
-      ).to_return(
-        body: file_fixture("crime_apply_data/applications/#{application_id}.json").read,
-        status: 200
-      )
-    end
 
     #
     # Stub for valid application for Kit Pound, fixture provided by the LaaCrimeSchemas Gem
--- spec/system/viewing_an_application/history_when_a_resubmission_spec.rb
+++ spec/system/viewing_an_application/history_when_a_resubmission_spec.rb
@@ -1,64 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe "Viewing a resubmitted application's history" do
-  let(:parent_id) { '5aa4c689-6fb5-47ff-9567-5efe7f8ac211' }
-  let(:application_id) { '148df27d-4710-4c5b-938c-bb132eb040ca' }
+  include_context 'with resubmitted application'
 
   before do
-    visit '/'
-
-    travel_to Time.zone.now.yesterday
-
-    user = User.create(
-      first_name: 'Fred',
-      last_name: 'Smitheg',
-      auth_oid: '976658f9-f3d5-49ec-b0a9-485ff8b308fa',
-      email: 'Fred.Smitheg@justice.gov.uk'
-    )
-
-    Reviewing::ReceiveApplication.new(
-      application_id: parent_id,
-      parent_id: nil,
-      submitted_at: Time.zone.now.to_s
-    ).call
-
-    Assigning::AssignToUser.new(
-      user_id: user.id,
-      to_whom_id: user.id,
-      assignment_id: parent_id
-    ).call
-
-    allow(DatastoreApi::Requests::UpdateApplication).to receive(:new) {
-      instance_double(DatastoreApi::Requests::UpdateApplication, call: {})
-    }
-
-    return_details = ReturnDetails.new(
-      reason: 'evidence_issue',
-      details: 'September bank statement required'
-    )
-
-    Reviewing::SendBack.new(
-      user_id: user.id,
-      application_id: parent_id,
-      return_details: return_details.attributes
-    ).call
-
-    travel_back
-
-    Reviewing::ReceiveApplication.new(
-      application_id: application_id,
-      parent_id: parent_id,
-      submitted_at: Time.zone.now.to_s
-    ).call
-
     visit history_crime_application_path(application_id)
   end
 
-  it 'includes a link to the previous version of the application in the application history' do
-    within('tbody.govuk-table__body') do
-      expect { click_on('Go to this version') }.to change { page.current_path }
-        .from(history_crime_application_path(application_id))
-        .to(crime_application_path(parent_id))
+  context 'when viewing the resubmitted application\'s history' do
+    let(:application_data) { super().deep_merge('parent_id' => parent_id) }
+
+    it 'includes a link to the previous version of the application in the application history' do
+      within('tbody.govuk-table__body') do
+        expect { click_on('Go to this version') }.to change { page.current_path }
+          .from(history_crime_application_path(application_id))
+          .to(crime_application_path(parent_id))
+      end
     end
   end
 
