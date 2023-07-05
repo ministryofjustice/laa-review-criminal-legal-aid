ac51b6535d099cf32188d71b6e7768b25ab4569d
timpeat <tim@timpeat.com>
2023-07-05 12:39:44 +0100

    Use Rails 7 default error reporting (#335)

--- app/notifiers/access_granted_notifier.rb
+++ app/notifiers/access_granted_notifier.rb
@@ -1,11 +1,8 @@
 class AccessGrantedNotifier
   def call(event)
-    email = User.find(event.data.fetch(:user_id)).email
-    NotifyMailer.access_granted_email(email).deliver_now
-    #
-    # Rescue and report exceptions
-    # Notifying should not block
-  rescue StandardError => e
-    Sentry.capture_exception(e)
+    Rails.error.handle do
+      email = User.find(event.data.fetch(:user_id)).email
+      NotifyMailer.access_granted_email(email).deliver_now
+    end
   end
 end
