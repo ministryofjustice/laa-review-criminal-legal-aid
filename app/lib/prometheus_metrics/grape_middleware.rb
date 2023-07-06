b5daf865b03c172b174fe1896939f656bc40a273
Mo Seedat <mohammed.seedat@digital.justice.gov.uk>
2023-07-06 17:05:12 +0100

    CRIMRE-384 Add namespace logging change
    As per PR 128
    
    https://github.com/ministryofjustice/laa-criminal-applications-datastore/pull/128/files

--- app/lib/prometheus_metrics/grape_middleware.rb
+++ app/lib/prometheus_metrics/grape_middleware.rb
@@ -2,20 +2,35 @@ module PrometheusMetrics
   require 'prometheus_exporter/middleware'
 
   class GrapeMiddleware < PrometheusExporter::Middleware
+    def default_labels(env, result)
+      if _api_request?(env)
+        {
+          controller: '(api)',
+          action: env['api.endpoint'].namespace,
+        }
+      else
+        super
+      end
+    end
+
     def custom_labels(env)
-      return unless env['api.endpoint']
+      return unless _api_request?(env)
 
       api_version = env['api.version'] || 'n/a'
-      api_namespace = env['api.endpoint'].namespace
       api_method = env['api.endpoint'].options[:method].first
 
       {
         api_version:,
-        api_namespace:,
         api_method:,
       }
     rescue StandardError
       nil
+    end
+
+    private
+
+    def _api_request?(env)
+      env['api.endpoint'].present?
     end
   end
 end
--- spec/lib/prometheus_metrics/grape_middleware_spec.rb
+++ spec/lib/prometheus_metrics/grape_middleware_spec.rb
@@ -1,6 +1,30 @@
 require 'rails_helper'
 
 describe PrometheusMetrics::GrapeMiddleware do
+  # rubocop:disable RSpec/VerifiedDoubles
+  let(:api_endpoint) do
+    double('api.endpoint', namespace: '/applications', options: { method: ['GET'] })
+  end
+  # rubocop:enable RSpec/VerifiedDoubles
+
+  describe '#default_labels' do
+    subject(:grape_middleware) { described_class.new(nil, { instrument: nil }).default_labels(env, nil) }
+
+    context 'when it is an API request' do
+      let(:env) do
+        { 'api.endpoint' => api_endpoint }
+      end
+
+      it { expect(grape_middleware).to eq({ controller: '(api)', action: '/applications' }) }
+    end
+
+    context 'when it is not an API request' do
+      let(:env) { {} }
+
+      it { expect(grape_middleware).to eq({ controller: 'other', action: 'other' }) }
+    end
+  end
+
   describe '#custom_labels' do
     subject(:grape_middleware) { described_class.new(nil, { instrument: nil }).custom_labels(env) }
 
@@ -12,27 +36,21 @@ describe PrometheusMetrics::GrapeMiddleware do
         }
       end
 
-      # rubocop:disable RSpec/VerifiedDoubles
-      let(:api_endpoint) do
-        double('api.endpoint', namespace: '/applications', options: { method: ['GET'] })
-      end
-      # rubocop:enable RSpec/VerifiedDoubles
-
       context 'when there is no version' do
         let(:api_version) { nil }
 
-        it { expect(grape_middleware).to eq({ api_method: 'GET', api_namespace: '/applications', api_version: 'n/a' }) }
+        it { expect(grape_middleware).to eq({ api_method: 'GET', api_version: 'n/a' }) }
       end
 
       context 'when there is version' do
         let(:api_version) { 'v1' }
 
-        it { expect(grape_middleware).to eq({ api_method: 'GET', api_namespace: '/applications', api_version: 'v1' }) }
+        it { expect(grape_middleware).to eq({ api_method: 'GET', api_version: 'v1' }) }
       end
     end
 
     context 'when it is not an API request' do
-      let(:env) { { 'api.endpoint' => nil } }
+      let(:env) { {} }
 
       it { expect(grape_middleware).to be_nil }
     end
