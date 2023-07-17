d9b3c136f49dda5a48d530b43d7cca748c2d164e
Mohammed Hassan Latif <106679087+MohammedHassanLatif93@users.noreply.github.com>
2023-07-17 16:32:20 +0100

    Updated controller to show if user has dormant account or not. Added redirection to manage_users screen

--- app/controllers/admin/manage_users/revive_users_controller.rb
+++ app/controllers/admin/manage_users/revive_users_controller.rb
@@ -4,8 +4,16 @@ module Admin
       before_action :set_active_user, only: [:edit]
 
       def edit
-        set_flash :user_revived, user_name: @user.name
-        redirect_to admin_manage_users_root_path
+        if @user.dormant?
+          # set revive_until equal to today = today +48 hours
+          # send email to dormant user
+          set_flash :user_revive, user_name: @user.name
+          redirect_to admin_manage_users_root_path
+        else
+          # redirect user to manage user screen with notification error
+          set_flash :revive_denied, success: false, user_name: @user.name
+          redirect_to admin_manage_users_root_path
+        end
       end
 
       private
---
+++ spec/system/manage_users/revive_user_spec.rb
@@ -0,0 +1,110 @@
+require 'rails_helper'
+
+RSpec.describe 'Revive user from manage users dashboard' do
+  include_context 'when logged in user is admin'
+
+  let(:mail_double) do
+    instance_double(ActionMailer::MessageDelivery, deliver_now: true)
+  end
+
+  before do
+    allow(NotifyMailer).to receive(:access_granted_email) { mail_double }
+
+    visit '/'
+    visit '/admin/manage_users'
+    click_on 'Revive'
+  end
+
+  it 'loads the correct page' do
+    heading = first('h1').text
+
+    expect(heading).to have_content 'Revive'
+  end
+
+  it 'allows a user with management access to be added' do
+    fill_in 'Email', with: 'john@example.com'
+    check 'Give access to manage other users'
+
+    click_button 'Invite'
+
+    row = find(:xpath, "//table[@class='govuk-table']//tr[contains(td[1], 'john@example.com')]")
+
+    expect(page).to have_text('john@example.com has been invited')
+    expect(row).to have_text('john@example.com Yes')
+  end
+
+  it 'notifies the invitee' do
+    email = 'jane@example.com'
+    fill_in 'Email', with: email
+
+    click_button 'Invite'
+
+    expect(NotifyMailer).to have_received(:access_granted_email).with(email)
+    expect(mail_double).to have_received(:deliver_now)
+  end
+
+  it 'allows a user without management access to be added' do
+    fill_in 'Email', with: 'jane@example.com'
+
+    click_button 'Invite'
+
+    row = find(:xpath, "//table[@class='govuk-table']//tr[contains(td[1], 'jane@example.com')]")
+
+    expect(page).to have_text('jane@example.com has been invited')
+    expect(row).to have_text('jane@example.com No')
+  end
+
+  describe 'logging the invitation in the users account history' do
+    before do
+      email = 'jane@example.com'
+      fill_in 'Email', with: email
+      click_button 'Invite'
+      click_link email
+    end
+
+    let(:cells) { page.first('table tbody tr').all('td') }
+
+    it 'describes the event' do
+      expect(cells[1]).to have_content 'User invited'
+    end
+
+    it 'includes the managers name' do
+      expect(cells.last).to have_content 'Joe EXAMPLE'
+    end
+  end
+
+  describe 'validations' do
+    it 'errors when no email is provided' do
+      check 'Give access to manage other users'
+      click_button 'Invite'
+
+      error_message = first('#user-email-error').text.squish
+
+      expect(error_message).to have_text('Please enter an email')
+    end
+
+    it 'errors when email is in the wrong format' do
+      fill_in 'Email', with: 'WRONG FORMAT'
+      check 'Give access to manage other users'
+      click_button 'Invite'
+
+      error_message = first('#user-email-error').text.squish
+
+      expect(error_message).to have_text('Invalid email format')
+    end
+
+    it 'errors if the user is not unique / already exists' do
+      add_two_of_the_same_user
+      error_message = first('#user-email-error').text.squish
+      expect(error_message).to have_text('User already exists')
+    end
+
+    def add_two_of_the_same_user
+      fill_in 'Email', with: 'jane@example.com'
+      click_button 'Invite'
+      click_on 'Invite a user'
+      fill_in 'Email', with: 'jane@example.com'
+      click_button 'Invite'
+    end
+  end
+end
