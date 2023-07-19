f1a42b2e7db2812207f660b437d8861e547f85ae
Mo Seedat <mohammed.seedat@digital.justice.gov.uk>
2023-07-20 00:11:07 +0100

    CRIMRE-416 Add nil check for user

--- app/lib/warden_hooks/revivable.rb
+++ app/lib/warden_hooks/revivable.rb
@@ -1,7 +1,7 @@
 module WardenHooks
   module Revivable
     Warden::Manager.after_set_user except: :fetch do |user, _warden, _options|
-      Authorising::Revive.new(user: user, user_manager_id: user.id).call if user.revive_until.present?
+      Authorising::Revive.new(user: user, user_manager_id: user.id).call if user&.revive_until.present?
     end
   end
 end
