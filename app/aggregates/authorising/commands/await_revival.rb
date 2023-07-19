cb46403aa4f8c391ce55ff555b24bb11842f4856
Mo Seedat <mohammed.seedat@digital.justice.gov.uk>
2023-07-20 00:00:14 +0100

    CRIMRE-416 Clean up code and address rubocop

--- app/aggregates/authorising/commands/await_revival.rb
+++ app/aggregates/authorising/commands/await_revival.rb
@@ -3,6 +3,15 @@ module Authorising
     attribute :user_manager_id, Types::Uuid
 
     def call
+      raise User::CannotAwaitRevival, 'User must be dormant' unless user.dormant?
+      raise User::CannotAwaitRevival, 'Cannot revive yourself' if user.id == user_manager_id
+
+      perform
+    end
+
+    private
+
+    def perform
       user.transaction do
         user.revive_until = Rails.configuration.x.auth.dormant_account_revive_ttl.from_now
         user.save!
@@ -11,8 +20,6 @@ module Authorising
         publish_event!
       end
     end
-
-    private
 
     def event
       RevivalAwaited.new(data: { user_id:, user_manager_id: })
--- app/controllers/admin/manage_users/revive_users_controller.rb
+++ app/controllers/admin/manage_users/revive_users_controller.rb
@@ -4,23 +4,27 @@ module Admin
       before_action :set_active_user, only: [:edit]
 
       def edit
-        if @user.dormant?
-          Authorising::AwaitRevival.new(
-            user: @user,
-            user_manager_id: current_user_id
-          ).call
-
-          set_flash :user_revive, user_name: @user.name
-        else
-          set_flash :revive_denied, success: false, user_name: @user.name
-        end
+        begin_revival
       rescue User::CannotAwaitRevival
-        set_flash :revive_denied, success: false, user_name: @user.name
+        denied
       ensure
         redirect_to admin_manage_users_root_path
       end
 
       private
+
+      def begin_revival
+        Authorising::AwaitRevival.new(
+          user: @user,
+          user_manager_id: current_user_id
+        ).call
+
+        set_flash :user_revive, user_name: @user.name
+      end
+
+      def denied
+        set_flash :revive_denied, success: false, user_name: @user.name
+      end
 
       def set_active_user
         @user = User.active.find(params[:id])
