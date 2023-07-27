a603ec12cba1091b025c2ee58ba587cd38427058
Mo Seedat <mohammed.seedat@digital.justice.gov.uk>
2023-07-27 01:06:17 +0100

    CRIMAP-508 Add user role concern
    Make existing can_manage_others a virtual attribute for backward compatibility

--- app/models/concerns/user_role.rb
+++ app/models/concerns/user_role.rb
@@ -8,5 +8,19 @@ module UserRole
       supervisor: 'supervisor',
       user_manager: 'user_manager'
     }
+
+    attribute :can_manage_others, :boolean, default: false
+  end
+
+  def can_manage_others=(value)
+    if value
+      self[:role] = 'user_manager'
+    else
+      self[:role] = 'caseworker'
+    end
+  end
+
+  def can_manage_others?
+    user_manager?
   end
 end
