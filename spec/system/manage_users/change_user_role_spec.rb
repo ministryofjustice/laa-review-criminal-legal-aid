bd534ef4d9e3d608ed9670b4d836211aa20743aa
Mo Seedat <mohammed.seedat@digital.justice.gov.uk>
2023-08-02 15:18:51 +0100

    CRIMAP-508 Update system specs

---
+++ spec/system/manage_users/change_user_role_spec.rb
@@ -0,0 +1,99 @@
+require 'rails_helper'
+
+RSpec.describe 'Change user role' do
+  include_context 'when logged in user is admin'
+  include_context 'with an existing user'
+
+  describe 'when user is activated' do
+    before do
+      active_user.update(role: 'supervisor')
+      visit '/admin/manage_users/active_users'
+
+      within user_row do
+        click_on('Change role')
+      end
+    end
+
+    let(:confirmation_button) do
+      find(:xpath, "//button[@type='submit']")
+    end
+
+    it 'shows the confirmation page' do
+      expect(page).to have_content "Are you sure you want to change Zoe Blogs's role from Supervisor to Caseworker?"
+    end
+
+    it 'shows notification' do
+      click_on 'Yes, change to Caseworker'
+
+      expect(page).to have_success_notification_banner(
+        text: "Zoe Blogs's role has been changed from Supervisor to Caseworker"
+      )
+    end
+  end
+
+  describe 'when user is deactivated' do
+    before do
+      user.update(
+        deactivated_at: Time.zone.now
+      )
+    end
+
+    context 'with UI based flow' do
+      before do
+        visit '/admin/manage_users/deactivated_users'
+      end
+
+      it 'does not show change role action' do
+        within user_row do
+          expect(page).not_to have_content 'Change role'
+          expect(page).to have_content 'Reactivate'
+        end
+      end
+    end
+
+    context 'with URL based flow' do
+      before do
+        visit "/admin/manage_users/change_roles/#{user.id}/edit"
+      end
+
+      it 'is not found' do
+        expect(page).to have_title 'Page not found'
+      end
+    end
+  end
+
+  describe 'when user is dormant' do
+    before do
+      user.update(
+        first_auth_at: 200.days.ago,
+        last_auth_at: 100.days.ago
+      )
+    end
+
+    context 'with UI based flow' do
+      before do
+        visit '/admin/manage_users/active_users'
+      end
+
+      it 'does not show change role action' do
+        within user_row do
+          expect(page).not_to have_content 'Change role'
+          expect(page).to have_content 'Revive'
+        end
+      end
+    end
+
+    context 'with URL based flow' do
+      before do
+        visit "/admin/manage_users/change_roles/#{user.id}/edit"
+      end
+
+      it 'shows warning' do
+        expect(page).to have_notification_banner(
+          text: "Unable to change #{user.name}'s role",
+          details: []
+        )
+      end
+    end
+  end
+end
