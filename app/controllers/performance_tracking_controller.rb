2d79e2a379f1430961a90c59bb7aee7973b22935
Mohammed Hassan Latif <106679087+MohammedHassanLatif93@users.noreply.github.com>
2023-08-08 11:25:36 +0100

    Made changes to code after PR review

--- app/controllers/performance_tracking_controller.rb
+++ app/controllers/performance_tracking_controller.rb
@@ -8,7 +8,7 @@ class PerformanceTrackingController < ServiceController
   # Users that can manage others can access this on staging for testing purposes.
   def require_supervisor!
     return if current_user.supervisor? && FeatureFlags.basic_user_roles.enabled?
-    return if FeatureFlags.allow_user_managers_service_access.enabled?
+    return if allow_user_managers_service_access?
 
     render status: :not_found, template: 'errors/not_found', layout: 'errors'
     false
