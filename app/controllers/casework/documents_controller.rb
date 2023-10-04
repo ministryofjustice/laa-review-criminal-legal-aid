09d72de9cc1f9164c1b6a4393c0624b96ee0d4a9
hibo.abdilaahi <51047911+hiboabd@users.noreply.github.com>
2023-10-04 12:02:35 +0100

    WIP download supporting evidence

---
+++ app/controllers/casework/documents_controller.rb
@@ -0,0 +1,37 @@
+module Casework
+  class DocumentsController < Casework::BaseController
+    before_action :set_crime_application, :set_document
+
+    class CannotDownloadUnlessAssigned < StandardError; end
+    class CannotDownload < StandardError; end
+
+    def download
+      raise CannotDownloadUnlessAssigned unless @crime_application.assigned_to?(current_user_id)
+      raise CannotDownload unless doc_uploaded_to_current_app?
+
+      presign_download = Datastore::Documents::Download.new(document: @document).call
+      redirect_to(presign_download.url, allow_other_host: true)
+    rescue CannotDownloadUnlessAssigned
+      set_flash(:cannot_download_unless_assigned, success: false)
+      redirect_to crime_application_path(params[:crime_application_id])
+    rescue CannotDownload
+      set_flash(:cannot_download, success: false)
+      redirect_to crime_application_path(params[:crime_application_id])
+    end
+
+    private
+
+    def set_crime_application
+      @crime_application = ::CrimeApplication.find(params[:crime_application_id])
+    end
+
+    def set_document
+      @document = @crime_application.supporting_evidence.find { |evidence| evidence.s3_object_key == params[:id] }
+    end
+
+    def doc_uploaded_to_current_app?
+      # To ensure users can only download documents that are part of the current application
+      @document.present?
+    end
+  end
+end
---
+++ app/services/datastore/documents/download.rb
@@ -0,0 +1,34 @@
+module Datastore
+  module Documents
+    class Download
+      PRESIGNED_URL_EXPIRES_IN = 15 # seconds
+
+      attr_accessor :document
+
+      def initialize(document:)
+        @document = document
+      end
+
+      def call
+        DatastoreApi::Requests::Documents::PresignDownload.new(
+          object_key:, expires_in:, response_content_disposition:
+        ).call
+      end
+
+      private
+
+      def object_key
+        @document.s3_object_key
+      end
+
+      def expires_in
+        PRESIGNED_URL_EXPIRES_IN
+      end
+
+      def response_content_disposition
+        # To force download of file rather than opening in another window
+        "attachment; filename=#{@document.filename}"
+      end
+    end
+  end
+end
