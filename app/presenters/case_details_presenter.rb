5378258789a66f4ad928214b02f52c923e4da47b
Mo Seedat <mohammed.seedat@digital.justice.gov.uk>
2023-10-30 14:17:54 +0000

    CRIMAP-654 Show first ever hearing only

--- app/presenters/case_details_presenter.rb
+++ app/presenters/case_details_presenter.rb
@@ -1,6 +1,8 @@
 require 'laa_crime_schemas'
 
 class CaseDetailsPresenter < BasePresenter
+  # first_court_hearing reflects any changes in the court location, not
+  # the actual first court hearing location.
   def first_court_hearing
     if legacy_application?
       t('values.not_asked')
@@ -19,15 +21,19 @@ class CaseDetailsPresenter < BasePresenter
     l(hearing_date, format: :compact)
   end
 
-  private
-
   def legacy_application?
-    is_first_court_hearing.blank? || first_court_hearing_name.blank?
+    is_first_court_hearing.blank?
   end
 
   def no_hearing_yet?
     is_first_court_hearing == type_of('no_hearing_yet')
   end
+
+  def first_court_hearing?
+    is_first_court_hearing == type_of('yes')
+  end
+
+  private
 
   def type_of(value)
     LaaCrimeSchemas::Types::FirstHearingAnswerValues[value]
