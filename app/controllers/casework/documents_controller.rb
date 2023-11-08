c4b75276605454526aeb05e1baffcdaa7297e75c
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2023-11-08 13:47:04 +0000

    [CRIMAP-736] Remove restriction allowing only assigned users to download evidence (#450)

--- app/controllers/casework/documents_controller.rb
+++ app/controllers/casework/documents_controller.rb
@@ -1,7 +1,7 @@
 module Casework
   class DocumentsController < Casework::BaseController
     before_action :set_crime_application, :set_document
-    before_action :require_assigned_user
+    before_action :require_doc_part_of_app
 
     class CannotDownloadUnlessAssigned < StandardError; end
     class CannotDownloadDocNotPartOfApp < StandardError; end
@@ -34,13 +34,9 @@ module Casework
       @document = @crime_application.supporting_evidence.find { |evidence| evidence.s3_object_key == params[:id] }
     end
 
-    def require_assigned_user
+    def require_doc_part_of_app
       # To ensure users can only download documents that are part of the current application
       raise CannotDownloadDocNotPartOfApp if @document.blank?
-      raise CannotDownloadUnlessAssigned unless @crime_application.assigned_to?(current_user_id)
-    rescue CannotDownloadUnlessAssigned
-      set_flash(:cannot_download_unless_assigned, success: false)
-      redirect_to crime_application_path(params[:crime_application_id])
     rescue CannotDownloadDocNotPartOfApp
       set_flash(:cannot_download_doc_uploaded_to_another_app, success: false)
       redirect_to crime_application_path(params[:crime_application_id])
