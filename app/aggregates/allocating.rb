8f0f8c2b83c5e46f5fa97e219bbeb4949e54d2c3
hibo.abdilaahi <51047911+hiboabd@users.noreply.github.com>
2023-11-14 14:57:45 +0000

    Remove duplicate calls to backward

--- app/aggregates/allocating.rb
+++ app/aggregates/allocating.rb
@@ -3,11 +3,11 @@ module Allocating
 
   class << self
     def user_competencies(user_id)
-      user_events(user_id).first&.data&.[](:competencies)
+      user_events(user_id).backward.first&.data&.[](:competencies)
     end
 
     def user_events(user_id)
-      Rails.application.config.event_store.read.stream(stream_name(user_id)).backward
+      Rails.application.config.event_store.read.stream(stream_name(user_id))
     end
 
     def stream_name(user_id)
