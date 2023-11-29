d03784bd7388e94f8a8d9aa4ee95878e1d2494c3
hibo.abdilaahi <51047911+hiboabd@users.noreply.github.com>
2023-11-29 14:03:50 +0000

    WIP add tests

--- spec/models/reporting/current_workload_report_spec.rb
+++ spec/models/reporting/current_workload_report_spec.rb
@@ -7,29 +7,43 @@ RSpec.describe Reporting::CurrentWorkloadReport do
     let(:arguments) { {} }
 
     before do
+      travel_to Time.zone.local(2023, 11, 28, 12)
+
+      # rubocop:disable Rails/SkipsModelValidations
       Review.insert_all([
-                          { application_id: SecureRandom.uuid, business_day: '2023-11-27', work_stream: 'criminal_applications_team', state: 'open' },
-                          { application_id: SecureRandom.uuid, business_day: '2023-11-27', work_stream: 'criminal_applications_team', state: 'sent_back' },
-                          { application_id: SecureRandom.uuid, business_day: '2023-11-27', work_stream: 'criminal_applications_team', state: 'completed' },
-                          { application_id: SecureRandom.uuid, business_day: '2023-11-28', work_stream: 'criminal_applications_team', state: 'open' },
-                          { application_id: SecureRandom.uuid, business_day: '2023-11-23', work_stream: 'criminal_applications_team', state: 'open' },
-                          { application_id: SecureRandom.uuid, business_day: '2023-11-15', work_stream: 'criminal_applications_team', state: 'open' },
-                          { application_id: SecureRandom.uuid, business_day: '2023-11-15', work_stream: 'extradition', state: 'open' },
+                          { application_id: SecureRandom.uuid, business_day: '2023-11-27',
+work_stream: 'criminal_applications_team', state: 'open' },
+                          { application_id: SecureRandom.uuid, business_day: '2023-11-27',
+work_stream: 'criminal_applications_team', state: 'sent_back' },
+                          { application_id: SecureRandom.uuid, business_day: '2023-11-27',
+work_stream: 'criminal_applications_team', state: 'completed' },
+                          { application_id: SecureRandom.uuid, business_day: '2023-11-28',
+work_stream: 'criminal_applications_team', state: 'open' },
+                          { application_id: SecureRandom.uuid, business_day: '2023-11-23',
+work_stream: 'criminal_applications_team', state: 'open' },
+                          { application_id: SecureRandom.uuid, business_day: '2023-11-15',
+work_stream: 'criminal_applications_team', state: 'open' },
+                          { application_id: SecureRandom.uuid, business_day: '2023-11-15', work_stream: 'extradition',
+state: 'open' },
 
                         ])
+      # rubocop:enable Rails/SkipsModelValidations
     end
 
-    it 'returns correct number of applications received' do
-      expect(rows).to eq [{ total_received: 3 }]
+    it 'returns the correct data for applications received' do
+      expect(rows.map(&:total_received)).to eq [1, 3, 0, 1, 0, 0, 0, 0, 0, 2]
+    end
 
-      [{total_received: }, {}, {}]
+    it 'returns the correct data for total open applications' do
+      expect(rows.map(&:total_open)).to eq [1, 1, 0, 1, 0, 0, 0, 0, 0, 2]
     end
 
     context 'when querying by work stream' do
       let(:arguments) { { work_streams: ['extradition'] } }
 
       it 'only shows counts for extradition work stream' do
-        expect(rows.last).to eq({ business_day: Date.new(2023, 11, 15), total_open: 1, total_received: 1})
+        expect(rows.map(&:total_received)).to eq [0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+        expect(rows.map(&:total_open)).to eq [0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
       end
     end
   end
