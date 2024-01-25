0c800f04b8fdb03ebfd5e139e168431fc57384c0
hibo.abdilaahi <51047911+hiboabd@users.noreply.github.com>
2024-01-25 13:28:05 +0000

    Edit application history display for PSE applications

--- spec/system/casework/viewing_an_application/post_submission_evidence_application_spec.rb
+++ spec/system/casework/viewing_an_application/post_submission_evidence_application_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe 'Viewing an application unassigned, open application' do
-  let(:application_id) { '21c37e3e-520f-46f1-bd1f-5c25ffc57d70' }
+  let(:crime_application_id) { '21c37e3e-520f-46f1-bd1f-5c25ffc57d70' }
   let(:application_data) { JSON.parse(LaaCrimeSchemas.fixture(1.0, name: 'post_submission_evidence').read) }
 
   before do
     stub_request(
       :get,
-      "#{ENV.fetch('DATASTORE_API_ROOT')}/api/v1/applications/#{application_id}"
+      "#{ENV.fetch('DATASTORE_API_ROOT')}/api/v1/applications/#{crime_application_id}"
     ).to_return(body: application_data.to_json, status: 200)
 
-    visit crime_application_path(application_id)
+    visit crime_application_path(crime_application_id)
   end
 
   it 'includes the application type' do
@@ -78,6 +78,39 @@ RSpec.describe 'Viewing an application unassigned, open application' do
                           "//table[@class='govuk-table app-dashboard-table govuk-!-margin-bottom-9']
                             //tr[contains(td[1], 'test.pdf')]")
       expect(evidence_row).to have_content('test.pdf Download file (pdf, 12 Bytes)')
+    end
+  end
+
+  context 'when viewing application history of a completed pse' do
+    before do
+      click_on('Application history')
+    end
+
+    it 'includes the submission event' do
+      first_row = page.first('.app-dashboard-table tbody tr').text
+      expect(first_row).to match('Monday 24 Oct 10:50 John Doe Post submission evidence submitted')
+    end
+
+    it 'includes the assigned event' do
+      click_on('Assign to your list')
+      click_on('Application history')
+
+      first_row = page.first('.app-dashboard-table tbody tr').text
+      expect(first_row).to match('Joe EXAMPLE Post submission evidence assigned to Joe EXAMPLE')
+    end
+
+    context 'with a completed pse application' do
+      include_context 'with a completed application'
+
+      before do
+        visit crime_application_path(crime_application_id)
+        click_on('Application history')
+      end
+
+      it 'includes the completed event' do
+        first_row = page.first('.app-dashboard-table tbody tr').text
+        expect(first_row).to match('Fred Smitheg Post submission evidence completed')
+      end
     end
   end
 end
