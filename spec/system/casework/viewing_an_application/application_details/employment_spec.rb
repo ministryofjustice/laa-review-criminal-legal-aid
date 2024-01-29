d12e4a8c73394da1556cb0782dd0bd0de3cee15a
hibo.abdilaahi <51047911+hiboabd@users.noreply.github.com>
2024-01-29 15:23:33 +0000

    Display employment section in application details

---
+++ spec/system/casework/viewing_an_application/application_details/employment_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+RSpec.describe 'Viewing the employment details of an application' do
+  include_context 'with stubbed application'
+
+  before do
+    visit crime_application_path(application_id)
+  end
+
+  context 'with employment details' do
+    it { expect(page).to have_content('Employment') }
+
+    it 'shows employment type' do
+      expect(page).to have_content("Client's employment status Not working")
+    end
+
+    it 'shows whether employment has ended in the last three months' do
+      expect(page).to have_content('Have they ended employment in the last 3 months? Yes')
+    end
+
+    it 'shows whether job was lost in custody and date details' do
+      expect(page).to have_content('Did they lose their job as a result of being in custody? Yes')
+      expect(page).to have_content('When did they lose their job? 01/09/2023')
+    end
+    
+    context 'when job was not lost in custody' do
+      let(:application_data) do
+        super().deep_merge('means_details' => { 'income_details' => { 'lost_job_in_custody' => 'no',
+                                                                      'date_job_lost' => nil } })
+      end
+
+      it 'does not show date custody lost details' do
+        expect(page).to have_content('Did they lose their job as a result of being in custody? No')
+        expect(page).not_to have_content('When did they lose their job?')
+      end
+    end
+  end
+
+  context 'with no income details' do
+    let(:application_data) do
+      super().deep_merge('means_details' => nil)
+    end
+
+    it 'does not show income section' do
+      expect(page).not_to have_content('Employment')
+    end
+  end
+end
