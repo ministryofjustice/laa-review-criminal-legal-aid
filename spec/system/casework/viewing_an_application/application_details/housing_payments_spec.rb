bfa36491313c5d3e9df4e2fefc9136319f4e2047
Mo Seedat <mohammed.seedat@digital.justice.gov.uk>
2024-03-19 14:23:05 +0000

    CRIMAPP-498 Working solution

--- spec/system/casework/viewing_an_application/application_details/housing_payments_spec.rb
+++ spec/system/casework/viewing_an_application/application_details/housing_payments_spec.rb
@@ -8,64 +8,100 @@ RSpec.describe 'Viewing the housing payments of an application' do
   end
 
   context 'with housing payments details' do
-    it { expect(page).to have_content('Employment') }
+    it { expect(page).to have_content('Housing payments') }
 
     context 'when housing payment is mortgage' do
       let(:application_data) do
-        super().deep_merge('means_details' => { 'income_details' => { 'lost_job_in_custody' => 'no',
-                                                                      'date_job_lost' => nil } })
+        super().deep_merge('means_details' => { 'outgoings_details' => { 'outgoings' => outgoings } })
       end
 
-      it 'does not show date custody lost details' do
-        expect(page).to have_content('Did they lose their job as a result of being in custody? No')
-        expect(page).not_to have_content('When did they lose their job?')
+      let(:outgoings) do
+        [{ payment_type: 'mortgage', amount: 10_001, frequency: 'annual', metadata: {} }]
+      end
+
+      it 'shows mortgage details' do
+        expect(page).to have_content('Which housing payment type? Mortgage')
+        expect(page).to have_content('How much mortgage do they pay? £100.01 every year')
       end
     end
 
     context 'when housing payment is rent' do
       let(:application_data) do
-        super().deep_merge('means_details' => { 'income_details' => { 'lost_job_in_custody' => 'no',
-                                                                      'date_job_lost' => nil } })
+        super().deep_merge('means_details' => { 'outgoings_details' => { 'outgoings' => outgoings } })
       end
 
-      it 'does not show date custody lost details' do
-        expect(page).to have_content('Did they lose their job as a result of being in custody? No')
-        expect(page).not_to have_content('When did they lose their job?')
+      let(:outgoings) do
+        [{ payment_type: 'rent', amount: 99_999, frequency: 'four_weeks', metadata: {} }]
+      end
+
+      it 'shows rent details' do
+        expect(page).to have_content('Which housing payment type? Rent')
+        expect(page).to have_content('How much rent do they pay? £999.99 every 4 weeks')
       end
     end
 
     context 'when housing payment is board_and_lodging' do
       let(:application_data) do
-        super().deep_merge('means_details' => { 'income_details' => { 'lost_job_in_custody' => 'no',
-                                                                      'date_job_lost' => nil } })
+        super().deep_merge('means_details' => { 'outgoings_details' => { 'outgoings' => outgoings } })
       end
 
-      it 'does not show date custody lost details' do
-        expect(page).to have_content('Did they lose their job as a result of being in custody? No')
-        expect(page).not_to have_content('When did they lose their job?')
+      let(:outgoings) do
+        [{
+          payment_type: 'board_and_lodging',
+          amount: 18_699,
+          frequency: 'fortnight',
+          metadata: {
+            'payee_name' => 'John Snow',
+            'payee_relationship_to_client' => 'Son',
+            'board_amount' => 19_999,
+            'food_amount' => 1_300,
+          }
+        }]
+      end
+
+      it 'shows board and lodgings details' do
+        expect(page).to have_content('Which housing payment type? Board and lodgings')
+        expect(page).to have_content('How much board and lodgings do they pay? £186.99 every 2 weeks')
+      end
+
+      it 'shows metadata' do
+        [
+          'How much does client pay for board and lodgings? £199.99 every 2 weeks',
+          'What is the name of the person client pays for board and lodgings? John Snow',
+          'What is client\'s relationship to this person? Son'
+        ].each { |line| expect(page).to have_content(line) }
       end
     end
 
-    context 'when council tax paymet is provided' do
+    context 'when council tax payment is provided' do
       let(:application_data) do
-        super().deep_merge('means_details' => { 'income_details' => { 'lost_job_in_custody' => 'no',
-                                                                      'date_job_lost' => nil } })
+        super().deep_merge('means_details' => {
+                              'pays_council_tax' => 'yes',
+                              'outgoings_details' => { 'outgoings' => outgoings }
+                            })
       end
 
-      it 'does not show date custody lost details' do
-        expect(page).to have_content('Did they lose their job as a result of being in custody? No')
-        expect(page).not_to have_content('When did they lose their job?')
+      let(:outgoings) do
+        [{ payment_type: 'council_tax', amount: 9_999, frequency: 'annual', metadata: {} }]
+      end
+
+      it 'shows council tax details' do
+        expect(page).to have_content('Does client pay Council Tax where they usually live? Yes')
+        expect(page).to have_content('How much council tax does client pay? £99.99 every year')
       end
     end
   end
 
   context 'with no housing payments details' do
     let(:application_data) do
-      super().deep_merge('means_details' => nil)
+      super().deep_merge('means_details' => {
+        'pays_council_tax' => 'yes',
+        'outgoings_details' => { 'outgoings' => [] }
+      })
     end
 
-    it 'does not show income section' do
-      expect(page).not_to have_content('Employment')
+    it 'still shows council tax' do
+      expect(page).to have_content('Does client pay Council Tax where they usually live? Yes')
     end
   end
 end
