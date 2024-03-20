91a82a665da4711f92b303bb03be3e362aae9566
Ripan <er.ripankumar@gmail.com>
2024-03-20 19:28:12 +0000

    Refactor

--- app/presenters/properties_presenter.rb
+++ app/presenters/properties_presenter.rb
@@ -2,10 +2,14 @@ require 'laa_crime_schemas'
 
 class PropertiesPresenter < BasePresenter
   OTHER = 'other'.freeze
+  RESIDENTIAL = 'residential'.freeze
+  COMMERCIAL = 'commercial'.freeze
+  LAND = 'land'.freeze
+
   PROPERTY_TYPE_MAPPING = {
-    'residential' => { display_name: 'property' },
-    'commercial' => { display_name: 'property' },
-    'land' => { display_name: 'land' }
+    residential: { display_name: 'property' },
+    commercial: { display_name: 'property' },
+    land: { display_name: 'land' }
   }.freeze
 
   def initialize(properties)
