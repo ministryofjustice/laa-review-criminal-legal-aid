c7092390f82fedea846e07d1c62f63d58ecad58a
hibo.abdilaahi <51047911+hiboabd@users.noreply.github.com>
2024-03-21 14:32:07 +0000

    Update address presentation and bump schema

--- app/presenters/income_payments_presenter.rb
+++ app/presenters/income_payments_presenter.rb
@@ -1,18 +1,4 @@
 class IncomePaymentsPresenter < BasePresenter
-  # NOTE: Remember to add any new types to this list otherwise it will not show on page edit
-  INCOME_PAYMENT_TYPES_ORDER = %w[
-    maintenance
-    private_pension
-    state_pension
-    interest_investment
-    student_loan_grant
-    board_from_family
-    rent
-    financial_support_with_access
-    from_friends_relatives
-    other
-  ].freeze
-
   def initialize(income_payments)
     super(
       @income_payments = income_payments
@@ -30,10 +16,14 @@ class IncomePaymentsPresenter < BasePresenter
   def ordered_payments
     return {} if @income_payments.empty?
 
-    INCOME_PAYMENT_TYPES_ORDER.index_with { |val| income_payment_of_type(val) }
+    income_payment_types.index_with { |val| income_payment_of_type(val) }
   end
 
   def income_payment_of_type(type)
     @income_payments.detect { |income_payment| income_payment.payment_type == type }
+  end
+
+  def income_payment_types
+    LaaCrimeSchemas::Types::IncomePaymentType.values
   end
 end
