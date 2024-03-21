3393417a88fd6ff9bdb17d4c7e2459120dd2f0ed
timpeat <tim@timpeat.com>
2024-03-21 12:37:00 +0000

    Move user scope to base controller

--- app/controllers/manage_competencies/base_controller.rb
+++ app/controllers/manage_competencies/base_controller.rb
@@ -18,5 +18,13 @@ module ManageCompetencies
 
       raise ForbiddenError, 'Must be a supervisor'
     end
+
+    def user_scope
+      scope = User.active.where(role: [Types::CASEWORKER_ROLE, Types::SUPERVISOR_ROLE])
+
+      return scope if FeatureFlags.allow_user_managers_service_access.enabled?
+
+      scope.where(can_manage_others: false)
+    end
   end
 end
