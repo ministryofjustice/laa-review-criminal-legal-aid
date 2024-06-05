ad8f57e7442fef7c0252e3673e52385146a2df24
timpeat <tim@timpeat.com>
2024-06-05 18:16:41 +0100

    CRIMAPP-945-add-partner-wages-qestion (#611)

--- spec/system/casework/viewing_an_application/application_details/savings_spec.rb
+++ spec/system/casework/viewing_an_application/application_details/savings_spec.rb
@@ -27,6 +27,8 @@ RSpec.describe 'Viewing the savings of an application' do
         page.find('h2.govuk-summary-card__title', text: 'Bank account').ancestor('div.govuk-summary-card')
       end
 
+      let(:partner_wages_text) { 'Partner’s wages or benefits paid into this account?' }
+
       it 'shows savings details' do # rubocop:disable RSpec/MultipleExpectations, RSpec/ExampleLength
         within(saving_card) do |card|
           expect(card).to have_summary_row 'Name of bank, building society or other holder of the savings', 'Halifax'
@@ -34,8 +36,23 @@ RSpec.describe 'Viewing the savings of an application' do
           expect(card).to have_summary_row 'Account number', '12345678'
           expect(card).to have_summary_row 'Account balance', '£2.00'
           expect(card).to have_summary_row 'Is the account overdrawn?', 'Yes'
-          expect(card).to have_summary_row 'Are client’s wages or benefits paid into this account?', 'Yes'
+          expect(card).to have_summary_row 'Client’s wages or benefits paid into this account?', 'Yes'
           expect(card).to have_summary_row 'Name the account is in', 'Client'
+          expect(card).not_to have_content partner_wages_text
+        end
+      end
+
+      context 'when partner wages information is given' do
+        let(:means_details) do
+          saving = super().dig('capital_details', 'savings').first
+          saving['are_partners_wages_paid_into_account'] = 'no'
+          super().deep_merge('capital_details' => { 'savings' => [saving] })
+        end
+
+        it 'shows savings details' do
+          within(saving_card) do |card|
+            expect(card).to have_summary_row partner_wages_text, 'No'
+          end
         end
       end
     end
