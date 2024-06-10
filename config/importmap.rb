d2b87fece9237b9eb39b5b6c0a513f43844770e0
timpeat <tim@timpeat.com>
2024-06-10 11:32:04 +0100

    Update govuk-frontent to 5.4.0

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,5 +2,5 @@
 
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "govuk-frontend", to: "https://ga.jspm.io/npm:govuk-frontend@5.2.0/dist/govuk/all.mjs"
+pin "govuk-frontend", to: "https://ga.jspm.io/npm:govuk-frontend@5.4.0/dist/govuk/all.mjs"
 pin_all_from "app/javascript/local", under: "local"
