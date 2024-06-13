bafef8935738e39ec3d4c70a9122c4773c66e1dc
Ripan <ripan.kumar@digital.justice.gov.uk>
2024-06-13 15:45:32 +0100

    CRIMAPP-875-refactor-self-assessment-tax-bill (#620)
    * Add self assessment tax bill attributes to income
    
    * Update income_details_spec.rb

--- spec/system/casework/viewing_an_application/application_details/income_details_spec.rb
+++ spec/system/casework/viewing_an_application/application_details/income_details_spec.rb
@@ -36,11 +36,17 @@ RSpec.describe 'Viewing the income details of an application' do
   end
 
   context 'with nil income details' do
+    let(:income_details_attributes) do
+      {
+        'income_above_threshold' => 'yes',
+        'has_frozen_income_or_assets' => nil,
+        'client_owns_property' => nil,
+        'has_savings' => nil
+      }
+    end
+
     let(:application_data) do
-      super().deep_merge('means_details' => { 'income_details' => { 'income_above_threshold' => 'yes',
-                                                                    'has_frozen_income_or_assets' => nil,
-                                                                    'client_owns_property' => nil,
-                                                                    'has_savings' => nil, } })
+      super().deep_merge('means_details' => { 'income_details' => income_details_attributes })
     end
 
     it 'shows income above threshold' do
@@ -57,6 +63,32 @@ RSpec.describe 'Viewing the income details of an application' do
 
     it 'shows savings or investments' do
       expect(page).to have_no_content('Has savings or investments?')
+    end
+  end
+
+  context 'with `self assessment` and `work benefits` in income details' do
+    let(:income_details_attributes) do
+      {
+        'applicant_other_work_benefit_received' => 'no',
+        'applicant_self_assessment_tax_bill' => 'yes',
+        'applicant_self_assessment_tax_bill_amount' => 555_00,
+        'applicant_self_assessment_tax_bill_frequency' => 'fortnight'
+      }
+    end
+    let(:application_data) do
+      super().deep_merge('means_details' => { 'income_details' => income_details_attributes })
+    end
+
+    it 'shows Other work benefits' do
+      expect(page).to have_content('Does your client receive any other benefits from work? No')
+    end
+
+    it 'shows Self Assessment tax' do
+      expect(page).to have_content('Does the client pay a Self Assessment tax bill? Yes')
+    end
+
+    it 'shows Self Assessment tax amount with frequency' do
+      expect(page).to have_content("Amount\n£555.00 every 2 weeks")
     end
   end
 end
