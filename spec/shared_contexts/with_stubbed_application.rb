2d46bba0a664e5355f3ed5ff8bd24593b51e1a80
timpeat <tim@timpeat.com>
2024-06-25 10:47:38 +0100

    CRIMAPP-1106 combine partner and client premium bonds (#627)

--- spec/shared_contexts/with_stubbed_application.rb
+++ spec/shared_contexts/with_stubbed_application.rb
@@ -1,11 +1,16 @@
-RSpec.shared_context 'with stubbed application' do
+RSpec.shared_context 'with stubbed application' do # rubocop:disable RSpec/MultipleMemoizedHelpers
   let(:application_id) { '696dd4fd-b619-4637-ab42-a5f4565bcf4a' }
   let(:income_details) { {} }
   let(:outgoings_details) { {} }
   let(:capital_details) { {} }
   let(:means_details) { { income_details:, outgoings_details:, capital_details: } }
+  let(:partner_included?) { false }
+
   let(:application_data) do
-    JSON.parse(LaaCrimeSchemas.fixture(1.0).read).deep_merge('means_details' => means_details.deep_stringify_keys)
+    JSON.parse(LaaCrimeSchemas.fixture(1.0).read).deep_merge(
+      'client_details' => { 'partner' => { 'is_included_in_means_assessment' => partner_included? } },
+      'means_details' => means_details.deep_stringify_keys
+    )
   end
 
   before do
--- spec/system/casework/viewing_an_application/application_details/premium_bonds_partner_spec.rb
+++ spec/system/casework/viewing_an_application/application_details/premium_bonds_partner_spec.rb
@@ -2,64 +2,84 @@ require 'rails_helper'
 
 RSpec.describe 'Viewing the Premium Bond details of the partner' do
   include_context 'with stubbed application'
-  let(:means_details) { JSON.parse(LaaCrimeSchemas.fixture(1.0, name: 'means').read) }
+
+  let(:partner_included?) { true }
 
   before do
     visit crime_application_path(application_id)
   end
 
-  context 'when the partner has premium bonds' do
-    # rubocop:disable Layout/LineLength
-    let(:application_data) do
-      super().deep_merge('means_details' => { 'capital_details' => { 'has_premium_bonds' => 'no',
-                                                                     'premium_bonds_total_value' => nil,
-                                                                     'premium_bonds_holder_number' => nil,
-                                                                     'partner_has_premium_bonds' => 'yes',
-                                                                     'partner_premium_bonds_total_value' => 200_000,
-                                                                     'partner_premium_bonds_holder_number' => '89548A' } })
-    end
-    # rubocop:enable Layout/LineLength
-
-    it 'shows the partner premium bonds card' do
-      expect(page).to have_css('h2.govuk-summary-card__title', text: 'Premium Bonds: partner')
+  context 'when the partner has premium bonds but the client does not' do
+    subject(:premium_bonds_card) do
+      page.find('h2.govuk-summary-card__title', text: 'Premium Bonds').ancestor('div.govuk-summary-card')
     end
 
-    describe 'a premium bonds card' do
-      subject(:premium_bonds_card) do
-        page.find('h2.govuk-summary-card__title', text: 'Premium Bonds: partner').ancestor('div.govuk-summary-card')
-      end
+    let(:capital_details) do
+      {
+        'has_premium_bonds' => 'no',
+        'premium_bonds_total_value' => nil,
+        'premium_bonds_holder_number' => nil,
+        'partner_has_premium_bonds' => 'yes',
+        'partner_premium_bonds_total_value' => 200_000,
+        'partner_premium_bonds_holder_number' => '89548A'
+      }
+    end
 
-      it 'shows the Premium bonds details' do # rubocop:disable RSpec/MultipleExpectations
-        within(premium_bonds_card) do |card|
-          expect(card).to have_summary_row 'Any Premium Bonds?', 'Yes'
-          expect(card).to have_summary_row 'Total value', '£2,000.00'
-          expect(card).to have_summary_row 'Holder number', '89548A'
-        end
+    it 'shows the Premium bonds details' do # rubocop:disable RSpec/MultipleExpectations
+      within(premium_bonds_card) do |card|
+        expect(card).to have_summary_row 'Premium Bonds: client', 'No'
+        expect(card).to have_summary_row 'Premium Bonds: partner', 'Yes'
+        expect(card).to have_summary_row 'Holder number: partner', '89548A'
+        expect(card).to have_summary_row 'Total value', '£2,000.00'
       end
     end
   end
 
-  context 'when the partner has no premium bonds' do
-    let(:means_details) { super().deep_merge('capital_details' => { 'partner_has_premium_bonds' => 'no' }) }
-
-    let(:application_data) do
-      super().deep_merge('means_details' => means_details)
+  context 'when both partner and client have premium bonds' do
+    subject(:premium_bonds_card) do
+      page.find('h2.govuk-summary-card__title', text: 'Premium Bonds').ancestor('div.govuk-summary-card')
     end
 
-    it 'shows the partner premium bonds card' do
-      expect(page).to have_css('h2.govuk-summary-card__title', text: 'Premium Bonds: partner')
+    let(:capital_details) do
+      {
+        'partner_has_premium_bonds' => 'yes',
+        'partner_premium_bonds_total_value' => 200_001,
+        'partner_premium_bonds_holder_number' => '89548A'
+      }
     end
 
-    describe 'a premium bonds card' do
-      subject(:premium_bonds_card) do
-        page.find('h2.govuk-summary-card__title', text: 'Premium Bonds: partner').ancestor('div.govuk-summary-card')
+    it 'shows the Premium bonds details' do # rubocop:disable RSpec/MultipleExpectations
+      within(premium_bonds_card) do |card|
+        expect(card).to have_summary_row 'Premium Bonds: client', 'Yes'
+        expect(card).to have_summary_row 'Holder number: client', '123568A'
+        expect(card).to have_summary_row 'Premium Bonds: partner', 'Yes'
+        expect(card).to have_summary_row 'Holder number: partner', '89548A'
+        expect(card).to have_summary_row 'Total value', '£3,000.01'
       end
+    end
+  end
 
-      it 'only shows the first question' do
-        within(premium_bonds_card) do |card|
-          expect(card).to have_summary_row 'Any Premium Bonds?', 'No'
-          expect(card.all('dd').size).to be 1
-        end
+  context 'when neither the partner or the client have premium bonds' do
+    subject(:premium_bonds_card) do
+      page.find('h2.govuk-summary-card__title', text: 'Premium Bonds').ancestor('div.govuk-summary-card')
+    end
+
+    let(:capital_details) do
+      {
+        'has_premium_bonds' => 'no',
+        'premium_bonds_total_value' => nil,
+        'premium_bonds_holder_number' => nil,
+        'partner_has_premium_bonds' => 'no',
+        'partner_premium_bonds_total_value' => 200_000,
+        'partner_premium_bonds_holder_number' => '89548A'
+      }
+    end
+
+    it 'only shows the first question' do # rubocop:disable RSpec/MultipleExpectations
+      within(premium_bonds_card) do |card|
+        expect(card).to have_summary_row 'Premium Bonds: client', 'No'
+        expect(card).to have_summary_row 'Premium Bonds: partner', 'No'
+        expect(card.all('dd').size).to be 2
       end
     end
   end
