1d5605c64ffc18920acbd00208baa6d943be2a6f
timpeat <tim@timpeat.com>
2024-08-29 16:04:17 +0100

    Comment out ignoring of healthcheck for debugging (#693)

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -5,13 +5,15 @@ Rails.application.configure do
   config.lograge.logger = ActiveSupport::Logger.new($stdout)
   config.lograge.formatter = Lograge::Formatters::Logstash.new
 
-  # Reduce noise in the logs by ignoring the healthcheck actions
-  config.lograge.ignore_actions = %w[
-    HealthcheckController#show
-    HealthcheckController#ping
-    DatastoreApi::HealthEngine::HealthcheckController#show
-    DatastoreApi::HealthEngine::HealthcheckController#ping
-  ]
+
+  # Commenting out ignoring of healthcheck to aid with debugging
+  # # Reduce noise in the logs by ignoring the healthcheck actions
+  # config.lograge.ignore_actions = %w[
+  #   HealthcheckController#show
+  #   HealthcheckController#ping
+  #   DatastoreApi::HealthEngine::HealthcheckController#show
+  #   DatastoreApi::HealthEngine::HealthcheckController#ping
+  # ]
 
   config.lograge.custom_options = lambda do |event|
     request = event.payload[:request]
