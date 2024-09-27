829d929989c00206d1e98c2e92d856b74eb462d0
timpeat <tim@timpeat.com>
2024-09-27 16:27:38 +0100

    Remove event browser

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  Rails.application.routes.draw { mount RailsEventStore::Browser => "/res" if Rails.env.development? }
   mount DatastoreApi::HealthEngine::Engine => '/datastore'
 
   get :health, to: 'healthcheck#show'
