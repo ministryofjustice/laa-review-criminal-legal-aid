8d739458e186ff87be2ef622583f0418d9b2fabf
timpeat <tim@timpeat.com>
2024-09-27 16:19:06 +0100

    Removing linking to submission history from this changeset

--- app/read_models/submission_history/configuration.rb
+++
@@ -1,9 +0,0 @@
-module SubmissionHistory
-  class Configuration
-    def call(event_store)
-      event_store.subscribe(
-        SubmissionHistory::LinkToSubmissionHistoryStream, to: SubmissionHistory::EVENT_TYPES
-      )
-    end
-  end
-end
