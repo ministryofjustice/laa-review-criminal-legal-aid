edc616fe8fff2cefc6b33c82a04f52560ae93ff7
hibo.abdilaahi <51047911+hiboabd@users.noreply.github.com>
2024-10-04 11:33:33 +0100

    Conditionally route completed applications

--- app/controllers/casework/completes_controller.rb
+++ app/controllers/casework/completes_controller.rb
@@ -11,8 +11,12 @@ module Casework
         decisions: @crime_application.draft_decisions.map(&:attributes)
       ).call
 
-      # set_flash :completed
-      redirect_to crime_application_complete_path(@crime_application)
+      if FeatureFlags.adding_decisions.enabled?
+        redirect_to crime_application_complete_path(@crime_application)
+      else
+        set_flash :completed
+        redirect_to assigned_applications_path
+      end
     rescue Reviewing::Error => e
       set_flash(e.message_key, success: false)
       redirect_to crime_application_path(@crime_application)
