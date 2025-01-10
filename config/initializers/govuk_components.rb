3ea6e0a784d131f2095c862744d38305d0aa473c
timpeat <tim@timpeat.com>
2025-01-10 13:30:20 +0000

    CRIMAPP-1525 update homepage URL

--- config/initializers/govuk_components.rb
+++ config/initializers/govuk_components.rb
@@ -1,7 +1,8 @@
 require 'settings'
 Govuk::Components.configure do |conf|
-  conf.default_header_service_name = "Review criminal legal aid applications"
-  conf.default_phase_banner_text = "This is a new service – your feedback will help us to improve it."
+  conf.default_header_homepage_url = 'https://www.gov.uk'
+  conf.default_header_service_name = 'Review criminal legal aid applications'
+  conf.default_phase_banner_text = 'This is a new service – your feedback will help us to improve it.'
   conf.default_phase_banner_tag = Settings.phase_banner_tag
   conf.default_tag_colour = Settings.default_tag_colour
 end
