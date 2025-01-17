45d07d989ee6a8511f8a3cd70259943a4927bca3
timpeat <tim@timpeat.com>
2025-01-17 09:09:13 +0000

    Remove unecessary setters

--- app/models/decisions/draft.rb
+++ app/models/decisions/draft.rb
@@ -1,12 +1,8 @@
 require 'laa_crime_schemas'
 module Decisions
   class Draft < LaaCrimeSchemas::Structs::Decision
-    attribute? :funding_decision, Types::Nil | Types::FundingDecision
-    attribute? :reference, Types::Nil | Types::Integer
     attribute? :decision_id, Types::Nil | Types::Integer | Types::Uuid
     attribute? :application_id, Types::Uuid
-    attribute? :maat_id, Types::Integer.optional
-    attribute? :case_id, Types::String.optional
     attribute? :court_type, Types::CourtType.optional
 
     def to_param
