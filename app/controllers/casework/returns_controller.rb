9a92833110db7e67c0539561e7a92f10b4a778cf
timpeat <tim@timpeat.com>
2025-02-11 09:24:05 +0000

    CRIMAPP-1589 unlink draft decisions when returning (#809)

--- app/controllers/casework/returns_controller.rb
+++ app/controllers/casework/returns_controller.rb
@@ -1,21 +1,19 @@
 module Casework
   class ReturnsController < Casework::BaseController
     before_action :set_crime_application
+    before_action :set_return_details
 
-    def new
-      @return_details = ReturnDetails.new
-    end
+    def new; end
 
     def create
-      @return_details = ReturnDetails.new(return_params)
-
       @return_details.validate!
 
-      Reviewing::SendBack.new(
-        application_id: params[:crime_application_id],
-        user_id: current_user_id,
-        return_details: @return_details.attributes
-      ).call
+      @crime_application.decision_ids.each do |decision_id|
+        Reviewing::RemoveDecision.call(application_id:, user_id:, decision_id:)
+      end
+
+      return_details = @return_details.attributes
+      Reviewing::SendBack.call(application_id:, user_id:, return_details:)
 
       flash_and_redirect :success, :sent_back
     rescue ActiveModel::ValidationError
@@ -25,6 +23,12 @@ module Casework
     end
 
     private
+
+    alias user_id current_user_id
+
+    def application_id
+      @crime_application.id
+    end
 
     def flash_and_redirect(key, message)
       flash[key] = I18n.t(message, scope: [:flash, key])
@@ -36,7 +40,13 @@ module Casework
     end
 
     def return_params
+      return {} unless params[:return_details]
+
       params[:return_details].permit(:reason, :details)
+    end
+
+    def set_return_details
+      @return_details = ReturnDetails.new(return_params)
     end
   end
 end
