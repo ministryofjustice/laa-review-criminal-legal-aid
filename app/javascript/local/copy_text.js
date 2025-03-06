7ab461b3d16cb273bc81f0977af126388bf5c5e1
lucas.shaw <lucas.shaw@digital.justice.gov.uk>
2025-03-06 08:18:38 +0000

    hide copy link if no Javascript. Read aloud copy confirmination with screen reader

--- app/javascript/local/copy_text.js
+++ app/javascript/local/copy_text.js
@@ -1,17 +1,21 @@
 function copyText(textElementId, copyLinkElementId) {
     let textElement = document.querySelector(textElementId);
     let copyLink = document.querySelector(copyLinkElementId);
-  
+    let referenceNumberAlert = document.getElementById("copy-reference-number-alert");
+
     if (textElement && copyLink) {
       copyLink.addEventListener('click', (e) => {
         e.preventDefault();
-  
+
         let text = textElement.textContent.trim();
+
         window.navigator.clipboard.writeText(text);
+        referenceNumberAlert.textContent = "Reference number copied";
+
         copyLink.blur();
         return true;
       });
     }
   }
-  
+
   export default copyText;
