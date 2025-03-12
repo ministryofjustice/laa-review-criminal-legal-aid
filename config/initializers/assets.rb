d915e705246b15a0b8135197921303973fe708e4
timpeat <tim@timpeat.com>
2025-03-12 09:19:12 +0000

    Use MoJ Frontend 4.0 (#829)

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,13 +1,15 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = "1.0"
+Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
+# moj-frontend uses absolute path
+Rails.application.config.assets.paths << Rails.root
 
 # Additional govuk branded assets like favicon, govuk-mask-icon, etc.
 Rails.application.config.assets.paths << Rails.root.join('node_modules/govuk-frontend/dist/govuk/assets')
