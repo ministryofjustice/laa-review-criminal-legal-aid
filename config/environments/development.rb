7e95faf6c88ecd36d33020c449ed8c5a0aad2dfe
timpeat <tim@timpeat.com>
2025-03-14 13:57:00 +0000

    Modernise asset pipeline
    Replace sprockets, dartsass, and importmap to use yarn modules
    as source for all css and js assets.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-require "active_support/core_ext/integer/time"
+require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
@@ -19,13 +19,13 @@ Rails.application.configure do
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
-  if Rails.root.join("tmp/caching-dev.txt").exist?
+  if Rails.root.join('tmp/caching-dev.txt').exist?
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
-      "Cache-Control" => "public, max-age=#{2.days.to_i}"
+      'Cache-Control' => "public, max-age=#{2.days.to_i}"
     }
   else
     config.action_controller.perform_caching = false
@@ -56,9 +56,6 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
-  # Suppress logger output for asset requests.
-  config.assets.quiet = true
-
   # Raises error for missing translations.
   config.i18n.raise_on_missing_translations = true
 
@@ -69,7 +66,7 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   # Add the development host if set
-  config.hosts += [ENV["DEVELOPMENT_HOST"]]
+  config.hosts += [ENV.fetch('DEVELOPMENT_HOST', nil)]
 
   # Allow connections from inside a docker container to this host machine
   config.hosts += %w[host.docker.internal]
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,18 +3,5 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
-# Add additional assets to the asset load path.
-# Rails.application.config.assets.paths << Emoji.images_path
-
-# Add Yarn node_modules folder to the asset load path.
-Rails.application.config.assets.paths << Rails.root.join('node_modules')
-# moj-frontend uses absolute path
-Rails.application.config.assets.paths << Rails.root
-
 # Additional govuk branded assets like favicon, govuk-mask-icon, etc.
 Rails.application.config.assets.paths << Rails.root.join('node_modules/govuk-frontend/dist/govuk/assets')
-
-# Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in the app/assets
-# folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
