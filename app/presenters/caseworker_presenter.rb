d7a80c90ca064aaa205837dd5615775b00ae7589
lucas.shaw <lucas.shaw@digital.justice.gov.uk>
2025-03-18 14:53:53 +0000

    use helper to format percentage values

--- app/presenters/caseworker_presenter.rb
+++ app/presenters/caseworker_presenter.rb
@@ -7,4 +7,8 @@ class CaseworkerPresenter < BasePresenter
       I18n.t('manage_competencies.caseworker_competencies.table_cells.no_competencies')
     end
   end
+
+  def format_percentage(value)
+    value.nil? ? nil : "#{value}%"
+  end
 end
