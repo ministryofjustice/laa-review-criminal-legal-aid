ec82ffe9832d4a9696c24097e4b27883b4261584
Hibo <51047911+hiboabd@users.noreply.github.com>
2025-03-27 09:14:40 +0000

    Amend weekly report date formatting (#849)

--- app/models/reporting/weekly_report.rb
+++ app/models/reporting/weekly_report.rb
@@ -6,9 +6,17 @@ module Reporting
 
     def period_text
       [
-        I18n.l(time_period.starts_on, format: '%A %-d %B'),
+        I18n.l(time_period.starts_on, format: start_date_format),
         I18n.l(time_period.ends_on, format: :reporting_long)
       ].join(' to ')
+    end
+
+    private
+
+    def start_date_format
+      return :reporting_long if time_period.starts_on.year != time_period.ends_on.year
+
+      '%A %-d %B'
     end
   end
 end
