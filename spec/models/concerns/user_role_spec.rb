8b12c176f590a7284245d3aba31fa59da77e5561
EdwinKruglov <edwin.kruglov@justice.gov.uk>
2025-05-14 11:07:48 +0100

    [CRIMAPP-1679] Limit access to Sidekiq Web

--- spec/models/concerns/user_role_spec.rb
+++ spec/models/concerns/user_role_spec.rb
@@ -269,4 +269,13 @@ RSpec.describe UserRole do
       end
     end
   end
+
+  describe '#admin?' do
+    context 'with can_manager_others attribute set to true' do
+      it 'returns true' do
+        user.can_manage_others = true
+        expect(user.admin?).to be true
+      end
+    end
+  end
 end
