081e02ca88abeaa46ba3bab58f363f5c2eca6848
Edwin Kruglov <edwin.kruglov@justice.gov.uk>
2025-08-07 16:11:15 +0100

    [CRIMAPP-1821] Limit message_poller logging (#913)

--- app/jobs/aws_message_processing_job.rb
+++ app/jobs/aws_message_processing_job.rb
@@ -1,6 +1,8 @@
 class AwsMessageProcessingJob < ApplicationJob
   queue_as :aws_messages
 
+  self.log_arguments = false
+
   def perform(message_id, message_body)
     message = Aws::SnsMessage.new(id: message_id, raw_body: message_body)
     Aws::MessageProcessor.process!(message)
