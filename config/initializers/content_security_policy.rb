d4cdd1210daad515d0e364b7da620b326fb71b79
timpeat <tim@timpeat.com>
2025-11-17 15:39:47 +0000

    [CRIMAPP-1921] Only same-origin resources are allowed.

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -6,21 +6,19 @@
 
 Rails.application.configure do
   config.content_security_policy do |policy|
-    policy.default_src :self, :https
-    policy.font_src    :self, :https, :data
-    policy.img_src     :self, :https, :data
+    policy.default_src :self
+    policy.font_src    :self
+    policy.img_src     :self
     policy.object_src  :none
-    policy.script_src  :self, :https
-    policy.style_src   :self, :https
+    policy.script_src  :self
+    policy.style_src   :self
     # Specify URI for violation reports
     # policy.report_uri "/csp-violation-report-endpoint"
   end
 
   # Generate session nonces for permitted importmap and inline scripts
   config.content_security_policy_nonce_generator = ->(request) { SecureRandom.base64(16) }
-
-  # config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
-  config.content_security_policy_nonce_directives = %w(script-src style-src)
+  config.content_security_policy_nonce_directives = %w[script-src style-src]
 
   # Report violations without enforcing the policy.
   # config.content_security_policy_report_only = true
