63a127b10a34c79b92e8cfca24e4be5ec94be7c6
lucas.shaw <lucas.shaw@digital.justice.gov.uk>
2026-03-30 12:23:06 +0100

    cover other non caseworkers

--- spec/aggregates/reviewing/reviewer_eligibility_spec.rb
+++ spec/aggregates/reviewing/reviewer_eligibility_spec.rb
@@ -7,6 +7,26 @@ RSpec.describe Reviewing::ReviewerEligibility do
   include_context 'with an existing supervisor user'
 
   let(:review) { instance_double(Reviewing::Review) }
+  let(:data_analyst_user) do
+    User.create!(
+      first_name: 'Data',
+      last_name: 'Analyst',
+      email: "data-analyst-#{SecureRandom.hex(4)}@example.com",
+      auth_subject_id: SecureRandom.uuid,
+      can_manage_others: false,
+      role: UserRole::DATA_ANALYST
+    )
+  end
+  let(:auditor_user) do
+    User.create!(
+      first_name: 'Audit',
+      last_name: 'User',
+      email: "auditor-#{SecureRandom.hex(4)}@example.com",
+      auth_subject_id: SecureRandom.uuid,
+      can_manage_others: false,
+      role: UserRole::AUDITOR
+    )
+  end
 
   context 'when the user is a caseworker' do
     let(:user) { caseworker_user }
@@ -16,8 +36,24 @@ RSpec.describe Reviewing::ReviewerEligibility do
     end
   end
 
-  context 'when the user is not a caseworker' do
+  context 'when the user is a supervisor' do
     let(:user) { supervisor_user }
+
+    it 'does not allow the user to review' do
+      expect(eligibility.allowed?).to be(false)
+    end
+  end
+
+  context 'when the user is a data analyst' do
+    let(:user) { data_analyst_user }
+
+    it 'does not allow the user to review' do
+      expect(eligibility.allowed?).to be(false)
+    end
+  end
+
+  context 'when the user is an auditor' do
+    let(:user) { auditor_user }
 
     it 'does not allow the user to review' do
       expect(eligibility.allowed?).to be(false)
