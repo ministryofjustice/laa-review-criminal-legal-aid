42d77a1dc767991a7ad648d33c2b9c38a63254cc
Alfred Mazimbe <Alfred.Mazimbe@justice.gov.uk>
2026-06-25 17:10:33 +0100

    CRIMAPP-2167: Improve coverage for frozen assets review specs

--- spec/system/casework/viewing_an_application/application_details/other_capital_details_spec.rb
+++ spec/system/casework/viewing_an_application/application_details/other_capital_details_spec.rb
@@ -40,5 +40,35 @@ RSpec.describe 'Viewing the other capital details of an application' do
         )
       end
     end
+
+    context 'when the answer is no' do
+      let(:application_data) do
+        super().deep_merge(
+          'means_details' => {
+            'capital_details' => {
+              'has_frozen_income_or_assets' => 'no'
+            }
+          }
+        )
+      end
+
+      let(:other_capital_card) do
+        page.find(
+          :xpath,
+          "//h3[normalize-space()='Other capital']/ancestor::div[@class='govuk-summary-card'][1]"
+        )
+      end
+
+      it 'shows whether client has income, savings or assets under a restraint or freezing order' do
+        within(other_capital_card) do
+          expect(page).to have_content(
+            'Income, savings or assets under a restraint or freezing order No'
+          )
+          expect(page).to have_no_content(
+            'Who does the restraint or freezing order relate to?'
+          )
+        end
+      end
+    end
   end
 end
