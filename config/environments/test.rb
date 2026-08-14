8cb7e4fe1e590788472020c7dd6772edecba9c26
EdwinKruglov <edwin.kruglov@justice.gov.uk>
2026-08-14 13:18:21 +0100

    Upgrade sidekiq and related dependencies

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,5 +1,5 @@
 require 'active_support/core_ext/integer/time'
-require 'sidekiq/testing'
+require 'sidekiq'
 
 # The test environment is used exclusively to run your application's
 # test suite. You never need to work with it otherwise. Remember that
@@ -66,5 +66,5 @@ Rails.application.configure do
 
   config.active_storage.service = :test
 
-  Sidekiq::Testing.inline!
+  Sidekiq.testing!(:inline)
 end
